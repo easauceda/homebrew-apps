836d0db65c9e498a5876021643796bdcae41e641
goreleaserbot <goreleaser@carlosbecker.com>
2017-11-16 20:40:02 -0800

    fib version 0.0.2-rc1

--- fib.rb
+++ fib.rb
@@ -3,7 +3,7 @@ class Fib < Formula
   homepage ""
   url "https://github.com/easauceda/fib/releases/download/0.0.2-rc1/fib_0.0.2-rc1_darwin_amd64.tar.gz"
   version "0.0.2-rc1"
-  sha256 "fe0e8bd7af1331c2dabcd3ef7f5d707f20f1b67c9835896290076d688df4466e"
+  sha256 "5203f0e3b9d636e8823c67c71023cd99b3a7229f3ac8d0d190f1c663f7f934eb"
 
   def install
     bin.install "fib"
